a1c339847f7cbb8cbb33963e923d19e57bc043a2
Valentin Volkl <valentin.volkl@cern.ch>
2016-05-07 19:18:50 +0200

    Add afs init scripts

---
+++ openafs.sh
@@ -0,0 +1,6 @@
+sudo apt-get install    openafs-client
+sudo apt-get install    openafs-modules-dkms
+sudo apt-get install    openafs-krb5
+sudo apt-get install    krb5-user
+sudo apt-get install    krb5-config
+sudo apt-get install    kstart
