de8245e5c6cfb8825156ba21a1355fca576e13e0
Valentin Volkl <valentin.volkl@cern.ch>
2016-05-18 19:27:43 +0200

    Package installs.

---
+++ root-packages.sh
@@ -0,0 +1,21 @@
+
+INSTALLCMD='sudo apt-get install'
+
+$INSTALLCMD procmail
+$INSTALLCMD git #for git command
+$INSTALLCMD cmake #for cmake tools
+$INSTALLCMD make #for make
+$INSTALLCMD freeglut3 #and freeglut3-dev for opengl for building coin3d
+$INSTALLCMD libncurses5
+$INSTALLCMD libncurses5-dev # for building mysql
+$INSTALLCMD libtool #needed for building libunwind
+$INSTALLCMD uuid-dev # needed for building xapian
+$INSTALLCMD libxml2-dev
+$INSTALLCMD ibxslt1-dev #needed for building all python required modules
+$INSTALLCMD libbz2-dev #needed for building boost
+$INSTALLCMD bison #and flex for doxygen
+$INSTALLCMD python-tk # for python tkiter module
+$INSTALLCMD byacc #needed by yacc for ftjam
+$INSTALLCMD libmotif-common
+$INSTALLCMD libmotif-common-dev #needed by Geant4
+$INSTALLCMD libxmu-headers # needed by Geant4 Use sudo apt-get install <package> or use the graphical "Synaptic Package Manager" program.
