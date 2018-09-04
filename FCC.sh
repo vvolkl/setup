97b02d6a8c94a58480d197fd86c6b4c9fd523fb5
Valentin Volkl <valentin.volkl@cern.ch>
2018-09-04 14:37:35 +0200

    up

--- FCC.sh
+++ FCC.sh
@@ -1,15 +1,15 @@
 export FCCPATH=$HOME/FCC
-source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-ubuntu1604-gcc54-opt/setup.sh 
+source /cvmfs/sft.cern.ch/lcg/views/LCG_93/x86_64-ubuntu1604-gcc54-opt/setup.sh 
 
 add_to_path CMAKE_PREFIX_PATH $FCCPATH/install
-add_to_path CMAKE_PREFIX_PATH $FCCPATH/Gaudi/InstallArea/x86_64-ubuntu1604-gcc54-dbg/
-add_to_path CMAKE_PREFIX_PATH /cvmfs/sft.cern.ch/lcg/releases/LCG_92/HepMC/2.06.09/x86_64-ubuntu1604-gcc54-opt
+add_to_path CMAKE_PREFIX_PATH $FCCPATH/Gaudi/InstallArea/x86_64-ubuntu1604-gcc54-opt/
+#add_to_path CMAKE_PREFIX_PATH /cvmfs/sft.cern.ch/lcg/releases/LCG_92/HepMC/2.06.09/x86_64-ubuntu1604-gcc54-opt
 
 
 export PODIO=$FCCPATH/install
 
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-ubuntu1604-gcc54-opt/bin/thisdd4hep_only.sh 
+source /cvmfs/sft.cern.ch/lcg/views/LCG_93/x86_64-ubuntu1604-gcc54-opt/bin/thisdd4hep_only.sh 
 
 export CMTPROJECTPATH=/cvmfs/sft.cern.ch/lcg/releases
 export BINARY_TAG=x86_64-ubuntu1604-gcc54-opt
