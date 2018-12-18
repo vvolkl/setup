e37b883b38c41d172f2b3b8903d8872a27714dff
Valentin Volkl <valentin.volkl@cern.ch>
2018-12-18 12:39:52 +0100

    up

--- ubuntu_setup_04_kerberos.sh
+++ ubuntu_setup_04_kerberos.sh
@@ -2,6 +2,8 @@ sudo apt-get install -y   krb5-user
 sudo apt-get install -y   krb5-config
 sudo apt-get install -y   kstart
 
+sudo cp -f ./krb5.conf /etc/krb5.conf
+
 user="vavolkl"
 
 printf "%b" "addent -password -p $user -k 1 -e aes256-cts\n$pass\naddent -password -p $user -k 1 -e arcfour-hmac-md5\n$pass\nwrite_kt $user.keytab" | ktutil
