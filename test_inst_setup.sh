20c12a034c8660c708a88bc4a47e5171c0fbee15
Valentin Volkl <valentin.volkl@cern.ch>
2019-05-02 15:49:50 +0200

    up

--- test_inst_setup.sh
+++ test_inst_setup.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-echo "export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH; export PATH=$1/bin:$PATH; export CPLUS_INCLUDE_DIR=$1/include:$CPLUS_INCLUDE_DIR; export PYTHONPATH=$1/python:$1/lib:$PYTHONPATH;"
+export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
+export PATH=$1/scripts:$1/bin:$PATH:
+export CPLUS_INCLUDE_DIR=$1/include:$CPLUS_INCLUDE_DIR
+export PYTHONPATH=$1/python:$1/lib:$PYTHONPATH
+export CMAKE_INSTALL_PREFIX=$1:$CMAKE_INSTALL_PREFIX
